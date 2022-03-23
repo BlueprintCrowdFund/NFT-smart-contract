2bf752fdaa1ff0de638cfce4fd1609d3b274256d
Abraham Onoja <35797372+L-tech@users.noreply.github.com>
2022-03-23 12:40:22 +0100

    Code optimisation and GitHub issues updates.

--- contracts/CampaignNFT.sol
+++ contracts/CampaignNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.10;
 
 // This constract is for creating NFT's for the Bluepring DAO on behalf of
 // the registering startup companies
